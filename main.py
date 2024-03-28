7fa91b455073995364aec5b8f4184e9eca98f678
Mihail <mishaaalexeev@yandex.ru>
2024-03-29 00:10:59 +0300

    Инициализация класса матрицы и дандер метод для сложения двух матриц

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+class Matrix:
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.m = len(matrix)
+        self.n = len(matrix[0])
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    def __add__(self, other):
+        result = []
+        if isinstance(other, Matrix):
+            if self.m == other.m and self.n == other.m:
+                for i in range(len(self.matrix)):
+                    row = []
+                    for j in range(len(self.matrix[0])):
+                        row.append(self.matrix[i][j] + other.matrix[i][j])
+                    result.append(row)
+            else:
+                print("Операция сложения для данных матриц не допустима")
+        elif isinstance(other, int):
+            for i in range(len(self.matrix)):
+                row = []
+                for j in range(len(self.matrix[0])):
+                    row.append(self.matrix[i][j] + other)
+                result.append(row)
+        else:
+            ValueError("")
+        return Matrix(result)
+
+    def __sub__(self, other):
+        print("minus")
+
+    def __mul__(self, other):
+        pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    mat = [[0 for _ in range(3)] for _ in range(3)]
+    mat1 = Matrix(mat)
+    print("Hello, world!")
