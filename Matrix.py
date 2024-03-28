2eab2a740ff3486780c25fe424fc9ad303aad660
Mihail <mishaaalexeev@yandex.ru>
2024-03-29 00:20:28 +0300

    Дандер метод вычитания

--- Matrix.py
+++ Matrix.py
@@ -4,6 +4,9 @@ class Matrix:
         self.m = len(matrix)
         self.n = len(matrix[0])
 
+    def __str__(self):
+        return '\n'.join(['\t'.join(map(str, row)) for row in self.matrix])
+
     def __add__(self, other):
         result = []
         if isinstance(other, Matrix):
@@ -26,7 +29,25 @@ class Matrix:
         return Matrix(result)
 
     def __sub__(self, other):
-        print("minus")
+        result = []
+        if isinstance(other, Matrix):
+            if self.m == other.m and self.n == other.m:
+                for i in range(len(self.matrix)):
+                    row = []
+                    for j in range(len(self.matrix[0])):
+                        row.append(self.matrix[i][j] - other.matrix[i][j])
+                    result.append(row)
+            else:
+                print("Операция сложения для данных матриц не допустима")
+        elif isinstance(other, int):
+            for i in range(len(self.matrix)):
+                row = []
+                for j in range(len(self.matrix[0])):
+                    row.append(self.matrix[i][j] - other)
+                result.append(row)
+        else:
+            ValueError("")
+        return Matrix(result)
 
     def __mul__(self, other):
         pass
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Matrix import *
 
 if __name__ == '__main__':
-    mat = [[0 for _ in range(3)] for _ in range(3)]
-    mat1 = Matrix(mat)
+    mat = [[3 for _ in range(3)] for _ in range(3)]
+    mat1 = Matrix([[3 for _ in range(3)] for _ in range(3)])
+    mat2 = Matrix([[1 for _ in range(3)] for _ in range(3)])
+    print(mat1 - mat2)
     print("Hello, world!")
